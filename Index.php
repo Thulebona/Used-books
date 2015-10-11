7efa6e7bfd2b7470928afd1c406a3032d438d819
2leradebe@gmail.com <2leradebe@gmail.com>
2015-10-11 19:59:59 +0200

    2nd commit

--- Index.php
+++ Index.php
@@ -19,47 +19,44 @@
        <li><a href='#'><span>Upload a Book</span></a></li>
        <li class='last'><a href='#'><span>Contact</span></a></li>
     </ul>
+
 </div>
 <table >
     <?php $i=0; 
 	while ($item = mysqli_fetch_object($result)) 
 	{?>
-             <td >
-                <table class="topic_table">
-                	<tr>
-                    	<td class="topic_head">
-                         <?php echo'<p align= "center" ><font color="#000000">'.$item->title.'</font></p>';?>
-                            </a>
-                        </td>
-                    <!--- / Topic Head / Title  /--->
-                    <!--- Topic Image --->
-                	</tr>
-                  	<tr>
-                    	<td class="topic_content">
-                    	<div id="theimages" class="imgWrap" align="center">
-                           <?php echo '<img src="images/'.$item->imageName.'" alt="'.$item->imageName.'">';?>
-                            <pre class="imgDescription">
-                               <?php echo '<p ><strong><b>' .$item->title.'</strong></b><br>'
-							    .$item->isbn.'<br>R:' .$item->price.
-							   '<br>' .$item-> bookCondition. '<br>' .$item->status. '</p>'  ?>
-                               
-                               <p> <input type="button" align="left" value="Add to Cut"  border="2px" class="addToCut"></p>
-                               
-                             </pre>
-            		   </div><br>
-                    </td>
-                </tr>
-                </table>
-             </td>
-        <?php
-         $i++;
-          if($i%6==0){   
+        <td >
+          <table class="topic_table">
+          	<tr>
+              	<td class="topic_head">
+                   <?php echo'<p align= "center" ><font color="#000000">'.$item->title.'</font></p>';?>
+                      </a>
+                </td>
+            </tr>
+            <tr>
+            	<td class="topic_content">
+            	   <div id="theimages" class="imgWrap" align="center">
+                       <?php echo '<img src="images/'.$item->imageName.'" alt="'.$item->imageName.'">';?>
+                        <pre class="imgDescription">
+                           <?php echo '<p ><strong><b>' .$item->title.'</strong></b><br>'.$item->isbn.'<br>R:' .$item->price.
+      			                '<br>' .$item-> bookCondition. '<br>' .$item->status. '</p><br><br><br><br>'  ?>
+                            
+                            <input type="button" value="Add to Cart"  border="2px" class="cartDiv"/> 
+                            <input type="button" value="Remove from Cart"  border="2px" class="cartDiv"/>
+                           
+                        </pre>
+                  </div><br>
+              </td>
+            </tr>
+          </table>
+      </td>
+      <?php $i++;
+        if($i%6==0){   
           echo "</tr>".PHP_EOL;
-			$i=0;
-         }
-		?>
-     <?php } ?>
+			    $i=0;
+        }
+      }
+		  ?>
 </table>
-
 </body>
 </html>
