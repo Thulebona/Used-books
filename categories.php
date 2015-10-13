e552ac6cb4404a484fb320ce4f621b7580e1dce6
2leradebe@gmail.com <2leradebe@gmail.com>
2015-10-13 12:00:13 +0200

    updated categories

--- categories.php
+++ categories.php
@@ -17,55 +17,12 @@
 </div>
 
 	<form align="center" class="container" action='index.php' method='post' accept-charset='UTF-8'  >
-		<a href="index.php"  ><button type='submit'  name='select' value="Business">
-		<span>Business</span>
-		<img src="images/category/business.png">
-		</button></a>
-
-		<a href="#"><button type='submit'  name='select' value="Sport Management">
-		<span>Sport Management		</span>
-		<img src="images/category/Sport Management.png">
-		</button></a>
-
-		<a href="#"><button type='submit'  name='select' value="Tourism">
-		<span>Tourism					</span>
-		<img src="images/category/Tourism.png">
-		</button></a>
-		
-		<a href="#"><button type='submit'  name='select' value="Informatics and design">
-		<span>Informatics and design	</span>
-		<img src="images/category/Informatics and design.png">
-		</button></a>
-
-		<a href="#"><button type='submit'  name='select' value="Accounting">
-		<span>Accounting</span>
-		<img src="images/category/Accounting.png">
-		</button></a>
-
-		<a href="#"><button type='submit'  name='select' value="Applied Science">
-		<span>Applied Science</span>
-		<img src="images/category/Applied Science.png">
-		</button></a>
-
-		<a href="#"><button type='submit'  name='select' value="Radiography">
-		<span>Radiography				</span>
-		<img src="images/category/Radiography.png">
-		</button></a>
-
-		<a href="#"><button type='submit'  name='select' value="Health and Wellness">
-		<span>Health and Wellness		</span>
-		<img src="images/category/Health and Wellness.png">
-		</button></a>
-
-		<a href="#"><button type='submit'  name='select' value="Architecture">
-		<span>Architecture			</span>
-		<img src="images/category/Architecture.png">
-		</button></a>
-
-		<a href="#"><button type='submit'  name='select' value="Hospitality">
-		<span>Hospitality				</span>
-		<img src="images/category/Architecture.png">
-		</button></a>
+	<?php 
+		$categories = array("Business", "Sport Management", "Tourism","Informatics and design","Accounting","Applied Science","Radiography","Health and Wellness","Architecture","Hospitality");
+		for($x = 0; $x < count($categories); $x++)  {
+	         echo '<button type="submit"  name="select" value="'.$categories[$x].'"><span>'.$categories[$x].'</span><img src="images/category/'.$categories[$x].'.png"></button>';
+	   }
+	 ?>
 	</form>
 
 </body>
